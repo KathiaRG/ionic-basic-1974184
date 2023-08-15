aa7a7ef51ee0048284755e2febdcdd505d3066ea
KathiaRG <kathia.rdz.grc@gmail.com>
2023-08-14 21:48:10 -0600

    Lunes1

---
+++ ionic-basic/src/app/presupuesto/presupuesto.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import {}
+@Component({
+  selector: 'app-presupuesto',
+  templateUrl: './presupuesto.page.html',
+  styleUrls: ['./presupuesto.page.scss'],
+})
+export class PresupuestoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ ionic-basic/src/app/servicio/presupuesto.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { Gasto } from '../interface/gasto';
+@Injectable({
+  providedIn: 'root'
+})
+export class PresupuestoService {
+
+  constructor() { }
+}
